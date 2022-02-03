5cfcc133494f83450725b063ce6d9722aa065bce
mehalforc <sdberndt@charter.net>
2022-02-03 04:20:30 -0600

    Add files via upload

--- gga-font-resizer/modules/Hooks.js
+++ gga-font-resizer/modules/Hooks.js
@@ -1,5 +1,4 @@
 import { warn, error, debug, log } from '../main.js'
-import { GFR } from './GFR.js'
 
 export let readyHooks = async () => {
   warn('Ready Hooks processing...')
@@ -7,14 +6,39 @@ export let readyHooks = async () => {
 }
 export const setupHooks = async () => {
   warn('Setup Hooks processing...')
-  GFR.init()
   Hooks.on('renderActorSheet', (app, html, data) => {
-    warn('Processing Actor Sheet...')
-    $('#qnotes')
-      .append('<hr/>')
-      .append("<a class='GFR'><i class='fas fa-plus-square'></i></a> ")
-      .append("<a class='GFR'><i class='fas fa-square'></i></a> ")
-      .append("<a class='GFR'><i class='fas fa-minus-square'></i></a>")
+    warn('Processing Actor Sheet...' + app)
+    let myFontSize = 1.0
+    let myFontSizeOther = 0.8
+    let myFontSizeResource = 0.65
+
+    const divElem = document.querySelector('#' + app.id)
+    const resizeObserver = new ResizeObserver(entries => {
+      for (let entry of entries) {
+        let theFontSize = ((($('#' + entry.target.id).width() / 800) * myFontSize - 1) * 0.1 + 1) * 100
+        let theFontSizeOther = ((($('#' + entry.target.id).width() / 800) * myFontSizeOther - 1) * 0.1 + 1) * 100
+        let theFontSizeResource = ((($('#' + entry.target.id).width() / 800) * myFontSizeResource - 1) * 0.1 + 1) * 100
+        $('#' + entry.target.id + ', #' + entry.target.id + ' *').css('font-size', theFontSize + '%')
+        $(
+          '#' +
+            entry.target.id +
+            ' .header, #' +
+            entry.target.id +
+            ' .points, #' +
+            entry.target.id +
+            ' .label, #portrait .sharebutton'
+        ).css('font-size', theFontSizeOther * 0.75 + '%')
+
+        $('#' + entry.target.id + ' .tracked-resource button').css('font-size', theFontSizeResource + '%')
+        $('#' + entry.target.id + ' .tracked-resource button').css('height', theFontSizeResource * 0.75 + '%')
+        $('#' + entry.target.id + ' .tracked-resource input').height(
+          $('#' + entry.target.id + ' .tracked-resource button').height()
+        )
+        $('#' + entry.target.id + ' .img-display').width($('#' + entry.target.id + ' #points').width() * 0.85)
+      }
+    })
+
+    resizeObserver.observe(divElem)
   })
 }
 export const initHooks = async () => {
